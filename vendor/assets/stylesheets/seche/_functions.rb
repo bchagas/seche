60848f7713c6bd6051f8d2f97dd23059039838e9
Bruno Chagas <brunochagasweb@gmail.com>
2012-06-06 00:48:44 -0300

    Removed wrong file

--- vendor/assets/stylesheets/seche/_functions.rb
+++
@@ -1,5 +0,0 @@
-@function container-width($width: $columns-layout)
-  @return ($width * $column-width) + (($width - 1) * $gutter-width)
-
-@function columns-width($n, $width: $columns-layout)
-  @return $n * $width + (($n - 1) * ($gutter-width * 2))
