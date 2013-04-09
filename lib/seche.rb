5d4931669d3046741b68fd3cec48e70e27e4fd03
Bruno Chagas <brunochagasweb@gmail.com>
2013-04-09 16:22:49 -0300

    Fixed sass load path

--- lib/seche.rb
+++ lib/seche.rb
@@ -7,7 +7,7 @@ module Seche
       end
     end
   else
-    Sass.load_paths << File.expand_path("../../app/assets/stylesheets", __FILE__)
+    Sass.load_paths << File.expand_path("../vendor/assets/stylesheets", __FILE__)
   end
 end
 
